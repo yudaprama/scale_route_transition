8699a41722150d86ad17acf600e3c8a75515036c
Yuda <akuntestinguntukseto@gmail.com>
2019-01-28 12:16:18 +0700

    init

---
+++ test/scale_route_transition_test.dart
@@ -0,0 +1,9 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:scale_route_transition/scale_route_transition.dart';
+
+void main() {
+  test('adds one to input values', () {
+    //
+  });
+}
